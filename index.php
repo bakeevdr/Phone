9f15e1072e29e4c6427dd0a2950a428d88ced7fb
BakeevDR <bakeev.dr@roslesinforg.ru>
2021-09-15 08:58:05 +0300

    --

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ require_once("messages.php");
 	<title><?php echo $Title ?></title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="icon" href="image/favicon.png" type="image/x-icon" />
-	<link rel="shortcut icon" href="image/favicon.png" type="image/x-icon" />
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/bootstrap.ais.css">
@@ -24,27 +24,6 @@ require_once("messages.php");
 			<script type='text/javascript' src="js/html5.js"></script>
 			<script type='text/javascript' src="js/respond.js"></script>
 		<![endif]-->
-	<!-- Matomo -->
-	<script type="text/javascript">
-		var _paq = _paq || [];
-		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		(function() {
-			var u = "//piwik.kadastr.ru/piwik/";
-			_paq.push(['setTrackerUrl', u + 'piwik.php']);
-			_paq.push(['setSiteId', '5']);
-			var d = document,
-				g = d.createElement('script'),
-				s = d.getElementsByTagName('script')[0];
-			g.type = 'text/javascript';
-			g.async = true;
-			g.defer = true;
-			g.src = u + 'piwik.js';
-			s.parentNode.insertBefore(g, s);
-		})();
-	</script>
-	<!-- End Matomo Code -->
 </head>
 
 <body>
